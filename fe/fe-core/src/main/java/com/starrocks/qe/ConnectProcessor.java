d0a217681729f9381d696278d99ac3f4bfa66388
andrewcheng <andrewcheng@tencent.com>
2022-08-15 10:13:56 +0800

    优化use db操作很慢的问题

--- fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ConnectProcessor.java
@@ -441,7 +441,7 @@ public class ConnectProcessor {
                 ctx.setStartTime();
                 break;
             case COM_FIELD_LIST:
-                handleFieldList();
+                //handleFieldList();
                 break;
             case COM_CHANGE_USER:
                 handleChangeUser();
