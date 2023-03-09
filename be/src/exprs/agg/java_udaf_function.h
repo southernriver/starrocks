ba346188c22a8a531f63569962bc20b7f0dd8c05
stdpain <34912776+stdpain@users.noreply.github.com>
2023-03-09 21:24:54 +0800

    [BugFix] Fix use wrong aggregate function context in no-group by aggregate (#19216)
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exprs/agg/java_udaf_function.h
+++ be/src/exprs/agg/java_udaf_function.h
@@ -415,12 +415,14 @@ public:
         RETURN_IF_UNLIKELY_NULL(res, (void)0);
         LOCAL_REF_GUARD_ENV(env, res);
         LogicalType type = udf_ctxs->finalize->method_desc[0].type;
+        // For nullable inputs, our UDAF does not produce nullable results
         if (!to->is_nullable()) {
             ColumnPtr wrapper(const_cast<Column*>(to), [](auto p) {});
             auto output = NullableColumn::create(wrapper, NullColumn::create());
             helper.get_result_from_boxed_array(ctx, type, output.get(), res, batch_size);
         } else {
             helper.get_result_from_boxed_array(ctx, type, to, res, batch_size);
+            down_cast<NullableColumn*>(to)->update_has_null();
         }
     }
 
