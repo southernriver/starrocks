823ef2290b81fdb0dc437df5e2bd46b028cf9b20
imay <buaa.zhaoc@gmail.com>
2023-06-13 15:08:51 +0800

    [Refactor] Change value of primitive type (#13705)

--- be/src/types/logical_type.h
+++ be/src/types/logical_type.h
@@ -38,6 +38,10 @@ enum LogicalType {
 
     // Reserved some field for commutiy version
 
+    LOGICAL_TYPE_NULL = 42,
+    LOGICAL_TYPE_FUNCTION = 43,
+    LOGCIAL_TYPE_TIME = 44,
+    LOGCIAL_TYPE_BINARY = 45,
     // decimal v3 type
     LOGICAL_TYPE_DECIMAL32 = 47,
     LOGICAL_TYPE_DECIMAL64 = 48,
@@ -122,6 +126,14 @@ inline const char* field_type_to_string(LogicalType type) {
         return "PERCENTILE";
     case LOGICAL_TYPE_JSON:
         return "JSON";
+    case LOGICAL_TYPE_NULL:
+        return "NULL";
+    case LOGICAL_TYPE_FUNCTION:
+        return "FUNCTION";
+    case LOGCIAL_TYPE_TIME:
+        return "TIME";
+    case LOGCIAL_TYPE_BINARY:
+        return "BINARY";
     case LOGICAL_TYPE_MAX_VALUE:
         return "MAX VALUE";
     }
